84e82a978703a541ac59a17bc5028a59d0d92c5c
Teagan Dorsch <teagandorsch@gmail.com>
2025-05-26 17:24:29 +0100

    added games and target model

---
+++ db/migrate/20250526162353_create_targets.rb
@@ -0,0 +1,11 @@
+class CreateTargets < ActiveRecord::Migration[7.2]
+  def change
+    create_table :targets do |t|
+      t.float :sleep
+      t.string :goal
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_05_26_161138) do
+ActiveRecord::Schema[7.2].define(version: 2025_05_26_162353) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "mode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "targets", force: :cascade do |t|
+    t.float "sleep"
+    t.string "goal"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_targets_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,4 +45,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_05_26_161138) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "targets", "users"
 end
