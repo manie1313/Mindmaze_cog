857b2e9aede779e9fba39d252984197d00f82be3
manel boulaabi <nuwr24@gmail.com>
2025-05-27 22:14:43 +0100

    Model val and asso

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,4 +1,9 @@
 class Performance < ApplicationRecord
   belongs_to :user
   belongs_to :game
+
+  validates :description, length: { in: 10..40 }
+  validates :description, presence: true
+  validates :accuracy,  numericality: { only_integer: true }
+  validates :completed, inclusion: [true, false]
 end
