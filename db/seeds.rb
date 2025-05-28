4f43ed418b01918519b22bd39b4b316247a8e2b1
Diana Culita <diana.culita100@gmail.com>
2025-05-28 16:30:01 +0100

    users seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,44 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+users_data = [
+  {
+    email: "diana.culita100@gmail.com",
+    password: "password",
+    first_name: "Diana",
+    last_name: "Culita",
+    username: "diana_di",
+    age: 26,
+    image_url: "https://res.cloudinary.com/dkb0r20o0/image/upload/c_thumb,w_200,g_face/v1747939607/mnljzu26h8hj59ovkzhl.jpg"
+  },
+  {
+    email: "mani@example.com",
+    password: "password",
+    first_name: "Manel",
+    last_name: "M",
+    username: "manel_bh",
+    age: 30,
+    image_url: "https://example.com/avatar/bob.png"
+  },
+  {
+    email: "Luke@example.com",
+    password: "password",
+    first_name: "Luke",
+    last_name: "Burton",
+    username: "luke_b",
+    age: 28,
+    image_url: "https://example.com/avatar/carol.png"
+  },
+  {
+  email: "Teagan@example.com",
+  password: "password",
+  first_name: "Teagan",
+  last_name: "Dorsch",
+  username: "Teagan_d",
+  age: 28,
+  image_url: "https://example.com/avatar/carol.png"
+}
+]
+
+users_data.map do |data|
+  User.create!(
+    data.except(:avatar).merge(image_url: data[:avatar])
+  )
+end
