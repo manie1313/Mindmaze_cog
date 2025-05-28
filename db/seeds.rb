5ef7abdcf465dfbbc46aab169a770b00d0dbcdaa
Diana Culita <diana.culita100@gmail.com>
2025-05-28 17:23:16 +0100

    Seed file add multiple users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,44 +1,59 @@
+# This file should ensure the existence of records required to run the application in every environment (production,
+# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Example:
+#
+#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
+#     MovieGenre.find_or_create_by!(name: genre_name)
+#   end
+
+require "open-uri"
+
+User.destroy_all
+
 users_data = [
   {
+    username: "diana_di",
     email: "diana.culita100@gmail.com",
     password: "password",
     first_name: "Diana",
     last_name: "Culita",
-    username: "diana_di",
     age: 26,
-    image_url: "https://res.cloudinary.com/dkb0r20o0/image/upload/c_thumb,w_200,g_face/v1747939607/mnljzu26h8hj59ovkzhl.jpg"
+    avatar_url: "https://res.cloudinary.com/dkb0r20o0/image/upload/c_thumb,w_200,g_face/v1747939607/mnljzu26h8hj59ovkzhl.jpg"
   },
   {
+    username: "manel_bh",
     email: "mani@example.com",
     password: "password",
     first_name: "Manel",
     last_name: "M",
-    username: "manel_bh",
-    age: 30,
-    image_url: "https://example.com/avatar/bob.png"
+    age: 26,
+    avatar_url: "https://example.com/avatar/bob.png"
   },
   {
+    username: "luke_b",
     email: "Luke@example.com",
     password: "password",
     first_name: "Luke",
     last_name: "Burton",
-    username: "luke_b",
-    age: 28,
-    image_url: "https://example.com/avatar/carol.png"
+    age: 26,
+    avatar_url: "https://example.com/avatar/carol.png"
   },
   {
-  email: "Teagan@example.com",
-  password: "password",
-  first_name: "Teagan",
-  last_name: "Dorsch",
-  username: "Teagan_d",
-  age: 28,
-  image_url: "https://example.com/avatar/carol.png"
-}
+    username: "Teagan_d",
+    email: "Teagan@example.com",
+    password: "password",
+    first_name: "Teagan",
+    last_name: "Dorsch",
+    age: 26,
+    avatar_url: "https://example.com/avatar/carol.png"
+  }
 ]
 
-users_data.map do |data|
-  User.create!(
-    data.except(:avatar).merge(image_url: data[:avatar])
-  )
+users_data.each do |data|
+  file = URI.parse(data[:avatar_url]).open
+  user = User.new(data.except(:avatar_url))
+  user.avatar.attach(io: file, filename: "#{data[:username]}.jpg", content_type: "image/jpg")
+  user.save!
 end
