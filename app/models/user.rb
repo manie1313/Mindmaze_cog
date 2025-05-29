6043352a934e1bd444a57afcf6266550c9a0a896
Diana Culita <diana.culita100@gmail.com>
2025-05-29 12:44:46 +0100

    User model added dependent destroy on targets

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_one_attached :avatar
 
   has_many :performances
-  has_many :targets
+  has_many :targets, dependent: :destroy
   has_many :games, through: :performances
   has_one_attached :avatar
 
