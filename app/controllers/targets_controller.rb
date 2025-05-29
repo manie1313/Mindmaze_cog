52c691d2c4a55a495afe2d5472897013ad88e237
Diana Culita <diana.culita100@gmail.com>
2025-05-29 12:42:36 +0100

    Targets controller

--- app/controllers/targets_controller.rb
+++ app/controllers/targets_controller.rb
@@ -1,3 +1,21 @@
 class TargetsController < ApplicationController
-  
+  before_action :authenticate_user!
+  def new
+    @target = Target.new
+  end
+
+  def create
+    @target = current_user.targets.new(target_params)
+    if @target.save
+      redirect_to @target, notice: "Target saved successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def target_params
+    params.require(:target).permit(:sleep, :goal)
+  end
 end
