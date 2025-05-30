21427ab530b6dc3cbc1639219bea0d98e4a64625
Diana Culita <diana.culita100@gmail.com>
2025-05-30 14:52:51 +0100

    Added targets in performances view and scss

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,10 +1,13 @@
 class PerformancesController < ApplicationController
+  before_action :authenticate_user!
   def index
     @performance = Performance.all
+    @targets = current_user.targets
   end
 
   def show
     # raise
     @performance = Performance.find(params[:game_id])
+    @targets = current_user.targets
   end
 end
