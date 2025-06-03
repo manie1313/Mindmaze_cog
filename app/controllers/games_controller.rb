b599cbd6e0207eaea64113675903d7c4063d5bdc
Diana Culita <diana.culita100@gmail.com>
2025-06-03 18:54:03 +0100

    Games controller added instance of performance. Added popup on the game show. Added form in performances new. Performances controller logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,8 +2,10 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
-  
+
   def show
     @game = Game.find(params[:id])
+    @performance = Performance.new
+    @target = current_user.targets.last
   end
 end
--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,29 +1,28 @@
 class PerformancesController < ApplicationController
   before_action :authenticate_user!
   before_action :set_target
-  before_action :set_game, only: [:new, :create, :show]
+  before_action :set_performance, only: [:show]
+
   def index
-   @performances = @target.performances.includes(:game)
+    @performances = @target.performances.includes(:game)
   end
 
   def new
-   @performance = @target.performances.new(game: @game)
+    @performance = @target.performances.new
   end
 
   def create
     @performance = @target.performances.new(performance_params)
-    @performance.game = @game
     @performance.completed = true
 
     if @performance.save
-      redirect_to target_performance_path(@target, @performance), notice: "Performance recorded!"
+      redirect_to target_performances_path(@target), notice: "Performance recorded!"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   def show
-    @performance = @target.performances.find(params[:id])
   end
 
   private
@@ -32,11 +31,11 @@ class PerformancesController < ApplicationController
     @target = current_user.targets.find(params[:target_id])
   end
 
-  def set_game
-    @game = Game.find(params[:game_id])
+  def set_performance
+    @performance = @target.performances.find(params[:id])
   end
 
   def performance_params
-    params.require(:performance).permit(:description, :accuracy, :time)
+    params.require(:performance).permit(:description, :accuracy, :time, :completed, :game_id)
   end
 end
