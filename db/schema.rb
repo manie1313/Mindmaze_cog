27d8465aa74ef60502a5e3b781cb78683879ede5
Diana Culita <diana.culita100@gmail.com>
2025-06-03 11:15:09 +0100

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -189,7 +189,7 @@ ActiveRecord::Schema[7.2].define(version: 2025_06_02_141627) do
   end
 
   create_table "targets", force: :cascade do |t|
-    t.float "sleep"
+    t.integer "sleep"
     t.string "goal"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
