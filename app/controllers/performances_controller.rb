8a0d0d37a969b3a86516996300c91e50f1130c4b
Diana Culita <diana.culita100@gmail.com>
2025-06-03 17:28:49 +0100

    Performances new and index

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -1,16 +1,42 @@
 class PerformancesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_target
+  before_action :set_game, only: [:new, :create, :show]
   def index
-    @performances = current_user.performances.includes(:game)
-    @targets = current_user.targets || []
-    @performances_today = current_user.performances.where(completed: true, created_at: Time.zone.today.all_day)
-    @today_target = @targets.select { |t| t.created_at.to_date == Date.today }.last
+   @performances = @target.performances.includes(:game)
+  end
+
+  def new
+   @performance = @target.performances.new(game: @game)
+  end
+
+  def create
+    @performance = @target.performances.new(performance_params)
+    @performance.game = @game
+    @performance.completed = true
+
+    if @performance.save
+      redirect_to target_performance_path(@target, @performance), notice: "Performance recorded!"
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @performance = @target.performances.find(params[:id])
+  end
+
+  private
+
+  def set_target
+    @target = current_user.targets.find(params[:target_id])
+  end
+
+  def set_game
     @game = Game.find(params[:game_id])
-    @performance = Performance.find(params[:id])
-    @targets = current_user.targets.includes(:performances) || []
-    @today_performance = @performances_today.last
+  end
+
+  def performance_params
+    params.require(:performance).permit(:description, :accuracy, :time)
   end
 end
