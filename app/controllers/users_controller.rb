62912b06f55597d0dcebc7a3ae69668afc513a51
Diana Culita <diana.culita100@gmail.com>
2025-06-05 12:07:48 +0100

    Homepage button lower. Scss for forms buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,12 @@
 class UsersController < ApplicationController
   def search
-    puts "[DEBUG] UsersController#search triggered"
-    puts "[DEBUG] params[:query]: #{params[:query].inspect}"
-
     query = params[:query].to_s.strip.downcase
 
     if query.present?
       users = User.where("username ILIKE ?", "%#{query}%").limit(10)
     else
-      puts "[DEBUG] Empty query received"
       users = []
     end
-
-    puts "[DEBUG] Found users: #{users.map(&:username)}"
 
     render json: users.map { |user|
       {
